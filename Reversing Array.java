4304f000fe640c6a34b9c65d403611003c4f9585
sahar540-lab <saharsami014@gmail.com>
2026-05-10 11:36:43 +0500

    Reversing array

---
+++ Reversing Array.java
@@ -0,0 +1,13 @@
+public class RevArr {
+    public static void main(String[] args) {
+        int arr[] = {10, 4, 6, 15, 16};
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
